7a5fac903009e3c772f2c1415a14e141738670b6
Althea Hanson <altheacodes@gmail.com>
2022-06-01 18:10:08 -0400

    declared variables and added my API key

---
+++ assets/JS/index.js
@@ -0,0 +1,8 @@
+var APIkey = "27a9f332092005bcc51e13fb84d6b86f";
+
+var cityInputEl = $('#city-input');
+var searchBtn = $('#search-button');
+var clearBtn = $('#clear-button');
+var pastSearchedCitiesEl = $('#past-searches');
+
+var currentCity;
